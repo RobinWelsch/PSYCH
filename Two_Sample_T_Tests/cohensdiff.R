a52ebeba866c7c86e87a994d27c130767919c63a
tuloist1 <tapio.tuloisela@aalto.fi>
2026-02-07 17:07:41 +0200

    Module 10: check formulas and syntax

--- Two_Sample_T_Tests/cohensdiff.R
+++ Two_Sample_T_Tests/cohensdiff.R
@@ -1,4 +1,4 @@
-cohensdiff <- "$d = \\frac{|\\bar{x}_1-\\bar{x}_2|}{\\sqrt{\\frac{((n_1-1)\\cdot s^2_1) + ((n_2-2)\\cdot s^2_2)}{n_1+n_2-2}}} \\\ d= \\frac{|\\bar{x}_1-\\bar{x}_2|}{\\sqrt{\\frac{((n_1-1)\\cdot s^2_1) + ((n_2-2)\\cdot s^2_2)}{n_1+n_2}}}$"
+cohensdiff <- "$d = \\frac{|\\bar{x}_1-\\bar{x}_2|}{\\sqrt{\\frac{((n_1-1)\\cdot s^2_1) + ((n_2-1)\\cdot s^2_2)}{n_1+n_2-2}}} \\\ d= \\frac{|\\bar{x}_1-\\bar{x}_2|}{\\sqrt{\\frac{((n_1-1)\\cdot s^2_1) + ((n_2-1)\\cdot s^2_2)}{n_1+n_2}}}$"
 plot.equation <- function(equation){
   require(latex2exp)
   plot(TeX(equation), cex=1.5)
